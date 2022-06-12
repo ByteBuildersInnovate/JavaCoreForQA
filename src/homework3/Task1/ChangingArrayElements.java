f5f635a664e5890fb16662728af7a7c424fcc1a6
smych <smychenkoiv@gmail>
2022-06-12 14:36:57 +0300

    homework3 - added two packages with homework implementation.

---
+++ src/homework3/Task1/ChangingArrayElements.java
@@ -0,0 +1,24 @@
+package homework3.Task1;
+
+import java.util.Arrays;
+
+public class ChangingArrayElements {
+    public static void main(String[] args) {
+
+        Integer arr1[] = {1,2,3,4,5,6,7};
+        String arr2[] = {"A", "B", "C", "D"};
+        swap(arr1,1,5);
+        swap(arr2,0,2);
+
+    }
+    public static void swap(Object[] arr, int n1, int n2) {
+        System.out.println("Массив до изменений: " + Arrays.toString(arr));
+        Object sw = arr[n1];
+        arr[n1] = arr[n2];
+        arr[n2] = sw;
+
+        System.out.println("Массив после изменений: " + Arrays.toString(arr));
+    }
+}
+
+
---
+++ src/homework3/Task2/Fruit.java
@@ -0,0 +1,5 @@
+package homework3.Task2;
+
+public abstract class Fruit {
+    abstract float getWeight();
+}
